fa98221f0fef10926978cd1de797fad44650c990
Jean Matias <smatias.jean@gmail.com>
2024-05-09 16:46:02 +0200

    include documentation

--- src/aiod/authorisation/authorisation.py
+++ src/aiod/authorisation/authorisation.py
@@ -15,27 +15,70 @@ keycloak_openid = KeycloakOpenID(
 
 
 def login(username: str, password: str) -> None:
+    """
+    Logs in the user with the provided username and password.
+
+    Args:
+        username (str): The username of the user.
+        password (str): The password of the user.
+
+    Raises:
+        MissingCredentialsError: If the username or password is missing.
+        MissingTokenError: If Keycloak returns an invalid token.
+    """
     if username is None or password is None:
-        raise Exception("User credentials missing, provide `username` and `password`")
+        raise MissingCredentialsError(
+            "Username and/or password missing! Please provide your credentials and try again."
+        )
     token = keycloak_openid.token(username, password)
 
     if not token:
-        raise Exception("Token error!")
+        raise MissingTokenError("Keycloak returned an invalid token!")
     os.environ["ACCESS_TOKEN"] = token["access_token"]
     os.environ["REFRESH_TOKEN"] = token["refresh_token"]
 
 
 def logout() -> None:
+    """
+    Logs out the current user.
+
+    Raises:
+        MissingTokenError: If the stored refresh token is empty.
+    """
     refresh_token = get_refresh_token()
-    if refresh_token:
-        keycloak_openid.logout(refresh_token)
-        os.environ.pop("ACCESS_TOKEN")
-        os.environ.pop("REFRESH_TOKEN")
+    if not refresh_token:
+        raise MissingTokenError(
+            "The stored refresh token is empty! Please try to login again."
+        )
+
+    keycloak_openid.logout(refresh_token)
+    os.environ.pop("ACCESS_TOKEN")
+    os.environ.pop("REFRESH_TOKEN")
 
 
 def get_access_token() -> str | None:
+    """
+    Retrieves the access token.
+
+    Returns:
+        str | None: The access token if available, else None.
+    """
     return os.getenv("ACCESS_TOKEN")
 
 
 def get_refresh_token() -> str | None:
+    """
+    Retrieves the refresh token.
+
+    Returns:
+        str | None: The refresh token if available, else None.
+    """
     return os.getenv("REFRESH_TOKEN")
+
+
+class MissingCredentialsError(Exception):
+    """Raised when username and/or password is/are missing during login."""
+
+
+class MissingTokenError(Exception):
+    """Raised when a token was failed to be retrieved."""
