d6e7a32a333fa107fa5f431935beab66007bcd6b
Jean Matias <smatias.jean@gmail.com>
2024-05-15 11:51:00 +0100

    create configurable config logic

--- src/aiod/__init__.py
+++ src/aiod/__init__.py
@@ -1,9 +1,10 @@
 from importlib.metadata import version
+from aiod.config.config import set_api_base_url, set_authentication_server, show_config
 from aiod.default.authorization_test import test as authorization_test
+from aiod.default.counts import asset_counts as counts
 from aiod.resources import case_studies
 from aiod.resources import computational_assets
 from aiod.resources import contacts
-from aiod.default.counts import asset_counts as counts
 from aiod.resources import datasets
 from aiod.resources import educational_resources
 from aiod.resources import events
--- src/aiod/config/config.py
+++ src/aiod/config/config.py
@@ -1,7 +1,50 @@
-import pathlib
-import tomllib
+from pathlib import Path
+import configparser
 
-with open(pathlib.Path(__file__).parent / "config.toml", "rb") as fh:
-    CONFIG = tomllib.load(fh)
 
-KEYCLOAK_CONFIG = CONFIG.get("keycloak", {})
+config_file = Path(__file__).parent / "config.ini"
+config = configparser.ConfigParser()
+config.read(config_file)
+
+
+def get_api_base_url() -> str:
+    return config["api"]["base_url"]
+
+
+def set_api_base_url(base_url: str):
+    config["api"]["base_url"] = base_url
+    write_config_file()
+
+
+def get_api_schema_latest_version() -> str:
+    return config["api"]["schema_latest_version"]
+
+
+def get_keycloak_server_url() -> str:
+    return config["keycloak"]["server_url"]
+
+
+def get_keycloak_realm() -> str:
+    return config["keycloak"]["realm"]
+
+
+def get_keycloak_client_id() -> str:
+    return config["keycloak"]["client_id"]
+
+
+def set_authentication_server(server_url: str):
+    config["keycloak"]["server_url"] = server_url
+    write_config_file()
+
+
+def show_config() -> dict[str, str]:
+    config_dict = {}
+    for section in config.sections():
+        for key, item in config.items(section):
+            config_dict[key] = item
+    return config_dict
+
+
+def write_config_file():
+    with open(config_file, "w") as f:
+        config.write(f)
