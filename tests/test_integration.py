3f1afe90c1e766b4fa3ade9ed56f41eb620b386e
PGijsbers <p.gijsbers@tue.nl>
2025-06-30 16:10:16 +0200

    Parametrize test

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -8,18 +8,15 @@ import pytest
 
 import aiod
 
-
-@pytest.mark.server()
-def test_get_dataset_from_default_version_server():
-    datasets = aiod.datasets.get_list()
-    assert len(datasets) == 10
-    dataset = aiod.datasets.get_asset(datasets["identifier"].iloc[0])
-    assert dataset["name"] == datasets["name"].iloc[0]
+DEFAULT_MARKER = "default"
+LATEST_VERSION_MARKER = ""
 
 
 @pytest.mark.server()
-def test_get_dataset_from_latest_version_server():
-    aiod.config.version = ""
+@pytest.mark.parametrize("version", [DEFAULT_MARKER, LATEST_VERSION_MARKER])
+def test_get_dataset_list_from_default_version_server(version: str):
+    if version == LATEST_VERSION_MARKER:
+        aiod.config.version = LATEST_VERSION_MARKER
     datasets = aiod.datasets.get_list()
     assert len(datasets) == 10
     dataset = aiod.datasets.get_asset(datasets["identifier"].iloc[0])
