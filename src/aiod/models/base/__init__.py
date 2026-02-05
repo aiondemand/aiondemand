3cd523dc494695d60e4347d4682dcb7f301da9c3
Franz Király <fkiraly@gcos.ai>
2026-02-05 01:06:50 +0100

    model indexing

---
+++ src/aiod/models/base/__init__.py
@@ -0,0 +1,5 @@
+"""Module with packaging adapters."""
+
+from aiod.models.base._base import _OpenmlModelPkg
+
+__all__ = ["_OpenmlModelPkg"]
---
+++ src/aiod/models/classification/auto_sklearn.py
@@ -0,0 +1,13 @@
+"""Auto-sklearn classifier."""
+
+from aiod.models.apis import _ModelPkgClassifier
+
+
+class OpenmlPkg__AutoSklearnClassifier(_ModelPkgClassifier):
+    _tags = {
+        "pkg_id": "AutoSklearnClassifier",
+        "python_dependencies": "auto-sklearn",
+        "pkg_pypi_name": "auto-sklearn",
+    }
+
+    _obj = "autosklearn.classification.AutoSklearnClassifier"
