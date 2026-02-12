1e7ce13d392ef5806b94f853f5670645d94db3f8
Eman Abdelhaleem <emanabdelhaleem4@gmail.com>
2026-02-12 08:54:43 +0200

    update imports

--- src/aiod/__init__.py
+++ src/aiod/__init__.py
@@ -1,22 +1,26 @@
 from aiod.configuration import config
 from aiod import taxonomies
 from aiod import bookmarks
-from aiod.resources import case_studies
-from aiod.resources import computational_assets
-from aiod.resources import contacts
-from aiod.resources import datasets
-from aiod.resources import educational_resources
-from aiod.resources import events
-from aiod.resources import experiments
-from aiod.resources import ml_models
-from aiod.resources import news
-from aiod.resources import organisations
-from aiod.resources import persons
-from aiod.resources import platforms
-from aiod.resources import projects
-from aiod.resources import publications
-from aiod.resources import services
-from aiod.resources import teams
+from aiod.resources import (
+    BaseResource,
+    SearchableMixin,
+    CaseStudies,
+    ComputationalAssets,
+    Contacts,
+    Datasets,
+    EducationalResources,
+    Events,
+    Experiments,
+    MLModels,
+    News,
+    Organisations,
+    Persons,
+    Platforms,
+    Projects,
+    Publications,
+    Services,
+    Teams,
+)
 
 from aiod.authentication import invalidate_token, get_current_user, create_token
 from aiod.default.counts import asset_counts as counts
---
+++ src/aiod/resources/__init__.py
@@ -0,0 +1,17 @@
+from .base_resource import BaseResource, SearchableMixin
+from .contacts import Contacts
+from .datasets import Datasets
+from .organisations import Organisations
+from .projects import Projects
+from .platforms import Platforms
+from .services import Services
+from .news import News
+from .experiments import Experiments
+from .events import Events
+from .educational_resources import EducationalResources
+from .computational_assets import ComputationalAssets
+from .ml_models import MLModels
+from .persons import Persons
+from .teams import Teams
+from .publications import Publications
+from .case_studies import CaseStudies
