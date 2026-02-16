5adc6c85db7c7c66fbbcee1fc35b8e46b0fba11e
Eman Abdelhaleem <emanabdelhaleem4@gmail.com>
2026-02-16 09:36:57 +0200

    pre-commit

--- src/aiod/resources/contacts.py
+++ src/aiod/resources/contacts.py
@@ -1,5 +1,5 @@
 from aiod.resources.base_resource import BaseResource, SearchableMixin
 
+
 class Contacts(BaseResource):
     asset_type = "contacts"
-
--- src/aiod/resources/datasets.py
+++ src/aiod/resources/datasets.py
@@ -1,6 +1,5 @@
-
 from aiod.resources.base_resource import BaseResource, SearchableMixin
+
 
 class Datasets(SearchableMixin, BaseResource):
     asset_type = "datasets"
-
