c0599af1a61ef34006c2857643c5b3c1e2de0776
Franz Király <fkiraly@gcos.ai>
2026-02-21 13:29:33 +0100

    Update test_get.py

--- src/aiod/models/tests/test_get.py
+++ src/aiod/models/tests/test_get.py
@@ -1,11 +1,10 @@
 """Tests for models.get."""
 
 import pytest
-
 from skbase.utils.dependencies import _check_soft_dependencies
 
 
-pytest.mark.skipif(not _check_soft_dependencies("scikit-learn", severity="none"))
+@pytest.mark.skipif(not _check_soft_dependencies("scikit-learn", severity="none"))
 def test_get_basic_usage():
     from sklearn.ensemble import RandomForestClassifier
 
