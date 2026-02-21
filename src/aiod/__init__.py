a6ae5df4bab85babdee755cc9d53023d03376297
Franz Király <fkiraly@gcos.ai>
2026-02-21 10:48:31 +0100

    path and import fixes

--- src/aiod/__init__.py
+++ src/aiod/__init__.py
@@ -4,6 +4,7 @@ from aiod.authentication import create_token, get_current_user, invalidate_token
 # from aiod.calls.calls import get_any_asset as get
 from aiod.configuration import config
 from aiod.default.counts import asset_counts as counts
+from aiod.models import get
 from aiod.resources import (
     case_studies,
     computational_assets,
--- src/aiod/models/apis/__init__.py
+++ src/aiod/models/apis/__init__.py
@@ -1,5 +1,5 @@
 """Module with packaging adapters."""
 
-from src.aiod.models.apis._sklearn_apis import _ModelPkgSklearnEstimator
+from aiod.models.apis._sklearn_apis import _ModelPkgSklearnEstimator
 
 __all__ = ["_ModelPkgSklearnEstimator"]
