8b908df1b887ac37593c26722769e49f240a203b
JATAYU000 <shrivaths44kunju@gmail.com>
2026-02-24 14:27:15 +0530

    move common utils

--- src/aiod/utils/_indexing/_preindex_sklearn.py
+++ src/aiod/utils/_indexing/_preindex_sklearn.py
@@ -142,32 +142,6 @@ def _all_sklearn_estimators(
     )
 
 
-def _generate_sklearn_objs_by_type(type_of_objs: dict) -> dict:
-    """
-    Generate _objs_by_type dictionary from _type_of_objs.
-
-    Args:
-        type_of_objs: Dictionary mapping object names to their types.
-                     Types can be strings or lists of strings for polymorphic objects.
-
-    Returns
-    -------
-        Dictionary mapping types to lists of object names.
-    """
-    objs_by_type: dict[str, list[str]] = {}
-
-    for obj_name, obj_types in type_of_objs.items():
-        if isinstance(obj_types, str):
-            obj_types = [obj_types]
-
-        for obj_type in obj_types:
-            if obj_type not in objs_by_type:
-                objs_by_type[obj_type] = []
-            objs_by_type[obj_type].append(obj_name)
-
-    return objs_by_type
-
-
 def _generate_sklearn_types_of_obj() -> dict:
     """
     Generate _type_of_objs dictionary from _all_sklearn_estimators.
