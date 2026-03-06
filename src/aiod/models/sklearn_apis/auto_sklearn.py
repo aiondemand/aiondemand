845e4e658e2f136a09d0ac3c3d48b3621dcf089d
JATAYU000 <shrivaths44kunju@gmail.com>
2026-03-06 13:30:47 +0530

    updated indexes

--- src/aiod/models/sklearn_apis/auto_sklearn.py
+++ src/aiod/models/sklearn_apis/auto_sklearn.py
@@ -3,11 +3,23 @@
 from aiod.models.apis import _ModelPkgSklearnEstimator
 
 
-class AiodPkg__AutoSklearnClassifier(_ModelPkgSklearnEstimator):
+class AiodPkg__AutoSklearn(_ModelPkgSklearnEstimator):
     _tags = {
-        "pkg_id": "AutoSklearnClassifier",
+        "pkg_id": "__multiple",
         "python_dependencies": "auto-sklearn",
-        "pkg_pypi_name": "auto-sklearn",
+        "pkg_pypi_name": "autosklearn",
+        "object_types": ["classifier", "regressor"],
     }
 
-    _obj = "autosklearn.classification.AutoSklearnClassifier"
+    _obj_dict = {
+        "AutoSklearnClassifier": "autosklearn.classification.AutoSklearnClassifier",
+        "AutoSklearnRegressor": "autosklearn.regression.AutoSklearnRegressor",
+    }
+    _type_of_objs = {
+        "AutoSklearnClassifier": "classifier",
+        "AutoSklearnRegressor": "regressor",
+    }
+    _objs_by_type = {
+        "classifier": ["AutoSklearnClassifier"],
+        "regressor": ["AutoSklearnRegressor"],
+    }
