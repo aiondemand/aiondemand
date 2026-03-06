9dfaa76f1a56dbb57e39564e83423b3394643c1b
geetu040 <raoarmaghanshakir040@gmail.com>
2026-03-06 16:40:51 +0500

    import other contracts

--- src/aiod/contracts/sklearn/__init__.py
+++ src/aiod/contracts/sklearn/__init__.py
@@ -1,8 +1,23 @@
 """Base class for scikit-learn API contracts."""
 
-from aiod.contracts.sklearn.contracts import classifier, estimator
+from aiod.contracts.sklearn.contracts import (
+    biclusterer,
+    classifier,
+    clusterer,
+    density,
+    estimator,
+    outlier_detector,
+    regressor,
+    transformer,
+)
 
 __all__ = [
     "estimator",
+    "regressor",
     "classifier",
+    "transformer",
+    "clusterer",
+    "biclusterer",
+    "density",
+    "outlier_detector",
 ]
